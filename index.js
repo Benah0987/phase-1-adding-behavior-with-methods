8aeb70475a098c7ec8cd71313cfb5d865afffb90
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-16 11:45:15 +0300

    tested some of these on the console

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  const myCat = new Cat("Whiskers", "male");
+console.log(myCat.speak()); // Whiskers says meow!
+
+const myDog = new Dog("Buddy", "female");
+console.log(myDog.speak()); // Buddy says woof!
+
+const myBird = new Bird("Tweety", "male");
+console.log(myBird.speak()); // It's me! Tweety, the parrot!
+
+const myBird2 = new Bird("Polly", "female");
+console.log(myBird2.speak()); // Polly says squawk!
